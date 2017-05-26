99eb02355e03d4757738987cee792af2ee30cba9
Ivan Kornilov <vano468@gmail.com>
2017-05-26 18:33:21 +0300

    Add urls validations form

---
+++ lib/url_shortener/urls/validations/form.rb
@@ -0,0 +1,20 @@
+require 'dry-validation'
+require 'uri'
+
+module UrlShortener
+  module Urls
+    module Validations
+      Form = Dry::Validation.Form do
+        configure do
+          config.messages_file = Container.root.join('config/locales/validation.yml')
+
+          def uri?(input)
+            !URI.regexp.match(input).nil?
+          end
+        end
+
+        required(:longUrl).filled(:uri?)
+      end
+    end
+  end
+end
